e32787cf6a91b09721b7142952680c74e7f27457
cc <jklosow@gwdg.de>
2019-06-03 16:26:18 +0200

    added plot rolls

--- randomNumber.py
+++ randomNumber.py
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     colour = get_colour_by_dice_roll(roll)
     write_log_file(outputfilename, colour)
     print("debug print")
-
+    plt.bar(range(6),rolls)
+    plt.show()
