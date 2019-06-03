804951f33bdd97467aff73f9e652159deee408b3
cc <jklosow@gwdg.de>
2019-06-03 14:48:30 +0200

    changed random number generator to color coded dice roll generator

--- coloursDice.py
+++ coloursDice.py
@@ -17,8 +17,25 @@ def write_log_file(outputfilename, data):
     f.write("My randomly generated number is " + str(data) + " (" + time.strftime("%H:%M:%S") + ")\n")
     f.close()
 
+def get_colour_by_dice(spots):
+
+    if spots == 1:
+	return "red"
+    if spots == 2:
+	return "blue"
+    if spots == 3:
+	return "black"
+    if spots == 4:
+	return "orange"
+    if spots == 5:
+	return "yellow"
+    if spots == 6:
+	return "green"
 
 if __name__ == "__main__":
     outputfilename = "randomNumber"
-    roll = get_random_number(1, 100)
-    write_log_file(outputfilename, roll)
+    roll = get_random_number(1, 6)
+    colour = get_colour_by_dice(roll)
+    write_log_file(outputfilename, colour)
+
+
